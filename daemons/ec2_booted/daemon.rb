a38726573fa7b0eab4bb2d327b14587c646dc467
John Coleman <john.coleman@wonga.com>
2013-07-23 15:00:15 +0100

    Move AWS configuration up from Daemons::Subscriber to the calling daemons.rb to allow testing without daemons/common/daemons.yml present

--- daemons/ec2_booted/daemon.rb
+++ daemons/ec2_booted/daemon.rb
@@ -3,14 +3,13 @@ require_relative '../common/subscriber'
 require_relative '../common/config'
 require_relative 'ec2_booted'
 
-subscriber = Daemons::Subscriber.new
-
+config = Daemons::Config.instance
+config.configure_aws
+sqs_poller = Daemons::Subscriber.new()
 case ARGV[0]
 when "run"
   begin
-    subscriber.subscribe(
-      Daemons::Config.instance['sqs']['booted_ec2_queue'],
-      Daemons::EC2Booted.new)
+    sqs_poller.subscribe(config['sqs']['booted_ec2_queue'],Daemons::EC2Booted.new(config))
   rescue => e
     puts "#{e}"
     retry
