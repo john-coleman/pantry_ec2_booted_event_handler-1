0e8ea72d4d3b894925f52b59342d06d917249618
Kieran Manning <kieran.manning@wonga.com>
2013-07-24 15:23:25 +0100

    Refactored ec2runner

--- daemons/ec2_runner/daemon.rb
+++ daemons/ec2_runner/daemon.rb
@@ -5,18 +5,20 @@ require_relative '../common/config'
 require_relative 'ec2_runner'
 
 config = Daemons.config
-config.configure_aws
+#config.configure_aws
 sqs_poller = Daemons::Subscriber.new()
+publisher = Publisher.new(config['sns']['topic_arn'])
+ec2 = AWS::EC2.new
 
 case ARGV[0]
 when 'run' 
-  begin
+  #begin
     sqs_poller.subscribe(
-      config['sqs']['boot_ec2_instance'],
-      Daemons::EC2BootInstanceHandler.new(config)
+      config['sqs']['boot_ec2_queue'],
+      Daemons::EC2Runner.new(ec2, publisher)
     )
-  rescue => e 
-    puts "#{e}"
-    retry
-  end
+  #rescue => e 
+  #  puts "#{e}"
+  #  retry
+  #end
 end
--- daemons/ec2_runner/ec2_runner.rb
+++ daemons/ec2_runner/ec2_runner.rb
@@ -1,39 +1,46 @@
 #!/usr/bin/env ruby
 
 require 'aws-sdk'
-require 'yaml'
 require 'json'
-require 'fog'
 require 'timeout'
+require_relative '../common/publisher'
 
 module Daemons
-  class EC2BootInstanceHandler
-    def initialize(config)
-      @config = config
-      @ec2 = AWS::EC2.new
+  class EC2Runner
+    def initialize(ec2, publisher)
+      @ec2 = ec2
+      @publisher = publisher
     end
 
-    def handle_message(msg_body)
-      msg_json = JSON.parse(msg_body)
-      existing_instance_id = machine_already_booted(msg_json["pantry_request_id"])
+    def handle_message(msg)
+      existing_instance_id = machine_already_booted(msg["pantry_request_id"])
       if !existing_instance_id
-        puts "Attempting to boot machine."
+        puts "Attempting to boot machine with id: #{msg["pantry_request_id"]}"
         instance_id = boot_machine(
-          msg_json["pantry_request_id"],
-          msg_json["instance_name"],
-          msg_json["flavor"],
-          msg_json["ami"],
-          msg_json["team_id"],
-          msg_json["subnet_id"],
-          msg_json["security_group_ids"]
+          msg["pantry_request_id"],
+          msg["instance_name"],
+          msg["flavor"],
+          msg["ami"],
+          msg["team_id"],
+          msg["subnet_id"],
+          msg["security_group_ids"]
         )
       else
         instance_id = existing_instance_id
+        puts "Machine request #{msg["pantry_request_id"]} already booted"
       end
       raise_machine_booted_event(
-        msg_json["pantry_request_id"],
+        msg["pantry_request_id"],
         instance_id        
       )
+    end
+
+    def raise_machine_booted_event(request_id, instance_id)
+      msg = {
+        request_id: request_id,
+        instance_id: instance_id
+      }
+      @publisher.publish(msg)      
     end
 
     def machine_already_booted(request_id)
@@ -46,52 +53,57 @@ module Daemons
     end
 
     def boot_machine(request_id, instance_name, flavor, ami, team_id, subnet_id, secgroup_ids)
-      instance = @ec2.instances.create(
-        image_id:         ami,
-        instance_type:    flavor_id,
-        count:            1,
-        security_groups:  secgroup_ids,
-        subnet:           subnet_id,
-      )
+      begin
+        instance = @ec2.instances.create(
+          image_id:             ami,
+          instance_type:        flavor,
+          count:                1,
+          security_group_ids:   [secgroup_ids],
+          subnet:               subnet_id,
+        )
+      rescue => e 
+        puts "Creating instance failed"
+        return false
+      end
       @ec2.client.create_tags(
-        resources: instance.id,
+        resources: [instance.id],
         tags: 
         [
           { key: "Name",              value: instance_name  },
-          { key: "team_id",           value: team_id        },
-          { key: "pantry_request_id", value: request_id     }
+          { key: "team_id",           value: "#{team_id}"   },
+          { key: "pantry_request_id", value: "#{request_id}"}
         ]
       )
-      puts instance.status
-
+      puts "instance status: pending"
       previous_status = nil
       status = Timeout::timeout(300){
         while true do 
-          # Valid values: ok | impaired | initializing | insufficient-data | not-applicable
-          sleep(1)
-          response = @ec2.client.describe_instance_status(instance_ids: [instance.id])
-          instance_status = response.data[:instance_status_set][0][:instance_status]
+          sleep(3)
+          begin
+            #Need to wait until machine is in a vague state of existence before status
+            response = @ec2.client.describe_instance_status(instance_ids: [instance.id])
+            instance_status = response.data[:instance_status_set][0][:instance_status][:status]
+          rescue
+            sleep(1)
+            retry
+          end
 
           if instance_status != previous_status
-            puts "#{instance_status}"
+            print "instance status: #{instance_status}"
             previous_status = instance_status
           end
-          print "."
 
           case instance_status
             when "initializing"
-              #Continue
+              print "."
             when "ok"
               return instance.id
             else
               raise "Unexpected EC2 status return: #{instance_status}"
           end
-
         end
       }
-      rescue => e
-        raise e 
-      end
+    end
   end
 end
 
