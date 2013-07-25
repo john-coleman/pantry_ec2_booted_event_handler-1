4dd6f237bd007ba384e432d3a03307d8ba8bc20a
John Coleman <john.coleman@wonga.com>
2013-07-25 11:17:41 +0100

    Make daemon.rb check if it's being called through a symlink

--- daemons/bootstrap_command_handler/daemon.rb
+++ daemons/bootstrap_command_handler/daemon.rb
@@ -5,19 +5,19 @@ require_relative '../common/subscriber'
 require_relative '../common/config'
 require_relative 'bootstrap_command_handler'
 
-config = Daemons::Config.initialize(File.join(File.dirname(__FILE__),"daemon.yml"))
+THIS_FILE = File.symlink?(__FILE__) ? File.readlink(__FILE__) : __FILE__
+config = Daemons::Config.new(File.expand_path(File.join(File.dirname(THIS_FILE),"daemon.yml")))
 daemon_config = {
   :backtrace => config['daemon']['backtrace'],
   :dir_mode => config['daemon']['dir_mode'].to_sym,
-  :dir => "#{config['daemon']['dir']}",
+  :dir => "#{File.expand_path(config['daemon']['dir'])}",
   :monitor => config['daemon']['monitor']
 }
-Daemons.run_proc(config['daemon']['app_name']) {
-#Daemons.run_proc(config['daemon']['app_name'], daemon_config) {
+Daemons.run_proc(config['daemon']['app_name'], daemon_config) {
   begin
     Daemons::Subscriber.new.subscribe(config['sqs']['queue_name'],Daemons::BootstrapCommandHandler.new(config['sns']['topic_arn']))
   rescue => e
     puts "#{e}"
-    #retry
+    retry
   end
 }
